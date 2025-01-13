93966b284111500a6ee407e169a338210aad4ffe
bisratawoke <bisratawoke52@gmail.com>
2025-01-13 11:58:30 +0300

    Implemented Patient serialzier

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Doctor
+from .models import Doctor,Patient
 
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class PatientSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Patient
         fields = '__all__'
 
 class DoctorSerialzier(serializers.ModelSerializer):
