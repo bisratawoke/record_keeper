12c163dc37497b0c27a3488b554a0ef2d1bbd11f
bisratawoke <bisratawoke52@gmail.com>
2025-01-13 16:48:58 +0300

    Implmented model for patient history

--- api/models.py
+++ api/models.py
@@ -14,3 +14,10 @@ class Patient(models.Model):
     primary_doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class PatientHistory(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
