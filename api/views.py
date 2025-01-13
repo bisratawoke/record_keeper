5f8447ae2f0a2e2a9a9672270eaacd34455c6905
bisratawoke <bisratawoke52@gmail.com>
2025-01-13 12:02:01 +0300

    Implemetend pateints view

--- api/views.py
+++ api/views.py
@@ -2,14 +2,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import Doctor
-from .serializers import DoctorSerialzier
+from .models import Doctor,Patient
+from .serializers import DoctorSerialzier,PatientSerializer
 
 
 class DoctorCrudView(APIView):
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
     def get(self,request):
         doctors = Doctor.objects.select_related("user")
         result = DoctorSerialzier(doctors,many=True)
@@ -25,3 +25,25 @@ class DoctorCrudView(APIView):
         
 
 
+
+
+class PatientCrudView(APIView):
+    
+    def get(self,request):
+        patients = Patient.objects.select_related("user")
+        data = PatientSerializer(patients,many=True)
+        return Response({
+            'data': {
+                'data': data
+            }
+        })
+
+    def post(self,request):
+        body = request.data
+        serialzier = PatientSerializer(data={**body,'user':request.user.id})
+
+        if serialzier.is_valid():
+            serialzier.save()
+            return Response(data={'message':'Patient created successfully'})
+        
+        return Response(data={'message':serialzier.errors})
